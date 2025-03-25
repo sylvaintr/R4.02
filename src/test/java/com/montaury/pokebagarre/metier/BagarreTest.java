e47c4609dd76e30af0e97ae4f47d796ebc535477
Sylvain Trouilh <strouilh@iutbayonne.univ-pau.fr>
2025-03-25 09:45:48 +0100

    realisationdes test de BagarreTest

---
+++ src/test/java/com/montaury/pokebagarre/metier/BagarreTest.java
@@ -0,0 +1,63 @@
+package com.montaury.pokebagarre.metier;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BagarreTest {
+
+    /*
+    - verifier aver le premier  nom pokemon null
+    - verifier aver le second  nom pokemon null
+    - verifier aver le second  nom pokemon chaine vide
+    - verifier aver le second  nom pokemon chaine vide
+    - verifier queles deux nom pokemeon soi parreille
+    - verifier si le premier nom est un vrai pokemon
+    - verifier si le seconde nom est un vrai pokemon
+     */
+
+    @Test
+    void le_premier_nom_est_null(){
+        //GIVEN
+        
+        // WHEN
+
+        // THEN
+    }
+    @Test
+    void le_second_nom_est_null(){
+        //GIVEN
+        // WHEN
+        // THEN
+    }
+    @Test
+    void le_premier_nom_est_une_chaine_vide(){
+        //GIVEN
+        // WHEN
+        // THEN
+    }
+    @Test
+    void le_second_nom_est_une_chaine_vide(){
+        //GIVEN
+        // WHEN
+        // THEN
+    }
+    @Test
+    void les_deux_nom_sont_egaux(){
+        //GIVEN
+        // WHEN
+        // THEN
+    }
+    @Test
+    void le_premier_nom_n_est_pas_un_vrai_nom_de_pokemon (){
+        //GIVEN
+        // WHEN
+        // THEN
+    }
+    @Test
+    void le_segonde_nom_n_est_pas_un_vrai_nom_de_pokemon(){
+        //GIVEN
+        // WHEN
+        // THEN
+    }
+}
